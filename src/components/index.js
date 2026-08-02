a267c2a0f86e4de75d4c43887a9dfb8cd755867c
Hey-Ismail <Heythereitsfahim@gmail.com>
2026-08-02 21:52:59 +0600

    feat(components): add component index barrel file for clean modular exports

---
+++ src/components/index.js
@@ -0,0 +1,10 @@
+export { default as Navbar } from './Navbar';
+export { default as HeroSection } from './HeroSection';
+export { default as StatsBanner } from './StatsBanner';
+export { default as CampaignsSection } from './CampaignsSection';
+export { default as HowItWorksSection } from './HowItWorksSection';
+export { default as TrustSection } from './TrustSection';
+export { default as TestimonialsSection } from './TestimonialsSection';
+export { default as CTASection } from './CTASection';
+export { default as Footer } from './Footer';
+export { GithubIcon, TwitterIcon, LinkedinIcon } from './BrandIcons';
